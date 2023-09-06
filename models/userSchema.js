f6fc2c52ade3877dc62289560ba1d652f239d06b
Satya Jeet rout <65801837+satyajeet-rout@users.noreply.github.com>
2023-09-06 13:55:37 +0000

    forgot password function added in model

--- models/userSchema.js
+++ models/userSchema.js
@@ -41,7 +41,7 @@ const userSchema = mongoose.Schema(
 
 //challang 1 - encrypt the password  -hooks
 userSchema.pre("save", async function(next){
-    if(!this.modified("password")) return next();
+    if(!this.isModified("password")) return next();
     this.password = await bcrypt.hash(this.password, 10)
     next()
 })
@@ -64,6 +64,21 @@ getJwtToken:  function(){
             expiresIn: config.JWT_EXPIRY
         }
     )
+},
+
+generateForgotPasswordToken : function(){
+    const forgotToken = crypto.randomBytes(20).toString('hex');
+
+    //step 1 - save to DB
+    this.forgotPasswordToken = crypto
+    .createHash("sha256")
+    .update(forgotToken)
+    .digest("hex")
+
+    this.forgotPasswordExpiry = Date.now() + 20 * 60 * 1000
+
+    //step 2 - return values to user
+    return forgotToken
 }
 
 }
