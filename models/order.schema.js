f3215d05d433acc71fd5decbab18fd9595d18c52
Satya Jeet rout <65801837+satyajeet-rout@users.noreply.github.com>
2023-09-07 11:50:02 +0000

    order schema added

---
+++ models/order.schema.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema(
+    {
+        products: {
+            type: [
+                {
+                    productId: {
+                        type: mongoose.Schema.Types.ObjectId,
+                        ref : "Product",
+                        required: true
+                    },
+                    count: Number,
+                    price: Number
+                }
+             ],
+            required: true
+        },
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        },
+        address: {
+            type: String,
+            required: true
+        },
+        phoneNumber: {
+            type: Number,
+            required: true
+        },
+        amount: {
+            type: Number,
+            required: true
+        },
+        coupon: String,
+        transactionId: String,
+        orderStatus:{
+            type: String,
+            enum: ["ORDERED", "SHIPPED", "DELIVERED", "CANCELLED"],
+            default: "ORDERED",
+            // CAN WE IMPROVE THIS ? ASSIGMNENT
+        },
+        //paymentMode: UPI, creditcard or wallet, COD
+        
+    },
+
+    {
+        timestamps: true
+    }
+)
+
+export default mongoose.model("order", orderSchema)
