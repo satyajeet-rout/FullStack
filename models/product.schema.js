22d595ad044c4b1f299d20d52f14d80ac4abdc7b
Satya Jeet rout <65801837+satyajeet-rout@users.noreply.github.com>
2023-09-07 10:51:05 +0000

    product schema added

--- models/product.schema.js
+++ models/product.schema.js
@@ -4,6 +4,50 @@ const productSchema = new mongoose.Schema(
     {
         name: {
             type:String,
+            required: [true, "Please provide a product name"],
+            trim: true,
+            maxLength: [120, "Product name should be a max of 120 characters"]
+        },
+
+    
+        price: {
+            type: Number,
+            required: [true, "Please provide a product name"],
+            maxLength: [5, "Product Price should be not be more than 5 digits"]
+        },
+
+
+        description: {
+            type:String,
+            //use some form of editor - personal assignment
+        },
+    
+        photos: [
+            {
+                secure_url: {
+                    type: String,
+                    required: true
+                }
+            }
+        ],
+        stock: {
+            type: Number,
+            default: 0
+        },
+        sold: {
+            type: Number,
+            default: 0
+        },
+        collectionID: {
+            type: mongoose.Schema.Types.ObjectId , //it is for storing collection id.
+            ref: "Collection"
         }
+
+    },
+
+    {
+        timestamps: true
     }
 )
+
+export default mongoose.model("Products", productSchema)
